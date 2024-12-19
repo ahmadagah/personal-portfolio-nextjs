485de147542e8b30e4648fb0338f361efe9fb76f
ahmad <zafarsafi98@gmail.com>
2024-12-19 08:03:18 -0800

    initial commit

---
+++ config/site.ts
@@ -0,0 +1,53 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: 'Next.js + NextUI',
+  description:
+    'Make beautiful websites regardless of your design experience.',
+  navItems: [
+    {
+      label: 'About',
+      href: '/about',
+    },
+    {
+      label: 'Projects',
+      href: '/projects',
+    },
+    {
+      label: 'Services',
+      href: '/services',
+    },
+    {
+      label: 'Blog',
+      href: '/blog',
+    },
+  ],
+  navMenuItems: [
+    {
+      label: 'About',
+      href: '/about',
+    },
+    {
+      label: 'Projects',
+      href: '/projects',
+    },
+    {
+      label: 'Services',
+      href: '/services',
+    },
+    {
+      label: 'Blog',
+      href: '/blog',
+    },
+    {
+      label: 'Logout',
+      href: '/logout',
+    },
+  ],
+  links: {
+    github: 'https://github.com/ahmadagah',
+    discord: 'https://discord.gg/_ahmad_pdx',
+    twitter: '#',
+    buyMeACoffee: '#',
+  },
+};
